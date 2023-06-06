dfc41aa031f503508744baba3edffe43e1b5e387
Rouillou <clemvarin081004@gmail.com>
2023-06-06 16:21:25 +0200

    formulaire

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
+import j2l.pytactx.agent as pytactx
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -13,17 +14,26 @@ class MainWindow(QtWidgets.QMainWindow):
         uic.loadUi("formulaire.ui", self)
 
     def onPseudoChange(self, pseudo):
-        print("Pseudo a changer", pseudo)
         self.pseudo = pseudo
     def onArenaChange(self, arena):
-        print("arena a changer", arena)
         self.arena = arena
     def onPasswordChange(self, password):
-        print("password a changer", password)
         self.password = password
 
     def onButtonClicked(self, Button):
-        self.close()
+        agent = pytactx.AgentFr(
+        nom= self.pseudo, 
+        arene=self.arena, 
+        username="demo",
+        password=self.password, 
+        url="mqtt.jusdeliens.com", 
+        verbosite=3
+        )
+
+        while True:
+            agent.orienter((agent.orientation+1)%4)
+            agent.actualiser()
+        
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
