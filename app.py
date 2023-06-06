3d3f5b3729015f1b33eb528539fa776f26a076e8
Rouillou <clemvarin081004@gmail.com>
2023-06-06 15:29:37 +0200

    formulaire

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import uic
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pseudo = ""
+        self.arena = ""
+        self.password = ""
+        uic.loadUi("formulaire.ui", self)
+
+    def onPseudoChange(self, pseudo):
+        print("Pseudo a changer", pseudo)
+        self.pseudo = pseudo
+    def onArenaChange(self, arena):
+        print("arena a changer", arena)
+        self.arena = arena
+    def onPasswordChange(self, password):
+        print("password a changer", password)
+        self.password = password
+
+    def onButtonClicked(self, Button):
+        self.close()
 
 app = QtWidgets.QApplication(sys.argv)
-
-window = uic.loadUi("formulaire.ui")
+window = MainWindow()
 window.show()
-app.exec()
+app.exec_()
